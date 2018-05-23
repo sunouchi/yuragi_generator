090545f777b2bdaf525c04ca115e8d1d797fc827
sunouchi <t@sunouchi.jp>
2018-05-23 15:17:15 +0900

    importをラクにできるようにする

--- yuragi/__init__.py
+++ yuragi/__init__.py
@@ -2,3 +2,6 @@ import os
 import sys
 # 別ディレクトリからでもこの階層にあるモジュールを呼び出すようにするため
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from .yuragi import Yuragi
+from .shortened_word import SingleShortenedWord, CombinedShortenedWord
